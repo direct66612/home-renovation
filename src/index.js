8517dfe47918517e6e6c8b41c4549d5631cd6a3a
direct66612 <113045624+direct66612@users.noreply.github.com>
2024-05-08 00:34:58 -0400

    init add to hosting this site

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/home-renovation">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
